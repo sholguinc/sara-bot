bb7141ade6f25d07b2698219ce326e29c28df77f
Sergio Holguin <sholguinc@uni.pe>
2023-02-22 18:11:51 -0500

    added home

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get, HttpCode } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get('/')
+  home() {
+    return { message: 'Hello World' };
+  }
+
   @Get('/health')
   @HttpCode(200)
   healthEndpoint() {
